73b9265f139fd5d1fb8dbad7dd34e3791518d05b
Ismael Muñoz <ismael17mu@gmail.com>
2023-09-22 02:27:35 -0300

    carrito y fun

--- logica.js
+++ logica.js
@@ -1,4 +1,6 @@
+let carrito = []
 
+const tableBody = document.getElementById('t-body')
 
 let artCards = document.getElementById("cards")
 artCards.classList.add('container')
@@ -6,11 +8,8 @@ artCards.classList.add('mx-auto')
 artCards.classList.add('gap-3')
 
 
-
-
-
 function stockProd(listaStock) {
-artCards.innerHTML = ''
+    artCards.innerHTML = ''
     for (const producto of listaStock) {
         artCards.innerHTML += `
     <div class="card" style="width: 18rem;">
@@ -24,21 +23,41 @@ artCards.innerHTML = ''
     </div>
     `;
     }
-//eventos
-let botones = document.getElementsByClassName('comprar')
-for(const boton of botones){
-    boton.addEventListener('click',()=>{
-        console.log('compraste'+boton.id)
-    })
-}
+    //evento
+    let botones = document.getElementsByClassName('comprar')
+    for (const boton of botones) {
+        boton.addEventListener('click', () => {
+            const alCarrito = listaStock.find((produc)=>produc.id == boton.id)
+            console.log(alCarrito)
+            itemCarrito(alCarrito)
+        })
+    }
 }
 stockProd(productos)
+
+function itemCarrito(produc){
+    carrito.push(produc)
+    console.table(carrito)
+
+    tableBody.innerHTML += `
+    <tr>
+        <td>${produc.id}</td>
+        <td>${produc.marca}</td>
+        <td>${produc.precio}</td>
+    </tr>
+    `;
+    const carritoTotal = carrito.reduce((acumulador, product)=>acumulador + product.precio, 0)
+    console.log(carritoTotal)
+    document.getElementById('total').innerText='Total: $ '+carritoTotal ;
+}
+
+
 
 let btnAdidas = document.getElementById('btn-adidas')
 btnAdidas.addEventListener('click', clickAdi)
 
-function clickAdi (){
-    clickAdi = productos.filter((prods)=>prods.marca == "Adidas")
+function clickAdi() {
+    clickAdi = productos.filter((prods) => prods.marca == "Adidas")
     console.log(clickAdi)
     stockProd(clickAdi)
 }
@@ -46,8 +65,8 @@ function clickAdi (){
 let btnNike = document.getElementById('btn-nike')
 btnNike.addEventListener('click', clickNik)
 
-function clickNik (){
-    clickNik = productos.filter((prods)=>prods.marca == "Nike")
+function clickNik() {
+    clickNik = productos.filter((prods) => prods.marca == "Nike")
     console.log(clickNik)
     stockProd(clickNik)
 }
@@ -55,8 +74,8 @@ function clickNik (){
 let btnPuma = document.getElementById('btn-puma')
 btnPuma.addEventListener('click', clickPum)
 
-function clickPum (){
-    clickPum = productos.filter((prods)=>prods.marca == "Puma")
+function clickPum() {
+    clickPum = productos.filter((prods) => prods.marca == "Puma")
     console.log(clickPum)
     stockProd(clickPum)
 }
@@ -64,8 +83,8 @@ function clickPum (){
 let btnRebook = document.getElementById('btn-rebook')
 btnRebook.addEventListener('click', clickReb)
 
-function clickReb (){
-    clickReb = productos.filter((prods)=>prods.marca == "Rebook")
+function clickReb() {
+    clickReb = productos.filter((prods) => prods.marca == "Rebook")
     console.log(clickReb)
     stockProd(clickReb)
 }
