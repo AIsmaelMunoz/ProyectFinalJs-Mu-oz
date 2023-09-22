4dbb94dd5274963e55a9a0d8f14ec86e73ce2e1e
Ismael Muñoz <ismael17mu@gmail.com>
2023-09-22 01:12:52 -0300

    correccion fun

--- logica.js
+++ logica.js
@@ -5,7 +5,7 @@ artCards.classList.add('container')
 artCards.classList.add('mx-auto')
 artCards.classList.add('gap-3')
 
-let btnAdidas = document.getElementById(adidas)
+
 
 
 
@@ -19,28 +19,56 @@ artCards.innerHTML = ''
             <h5 class="card-title">${producto.marca}</h5>
             <p class="card-text">Talle: ${producto.talle}</p>
             <p class="card-text">Precio: $${producto.precio}</p>
-            <button id=${producto.id}  class="btn btn-primary">Comprar</button>
+            <button id=${producto.id}  class="btn btn-primary comprar">Comprar</button>
         </div>
     </div>
     `;
     }
+//eventos
+let botones = document.getElementsByClassName('comprar')
+for(const boton of botones){
+    boton.addEventListener('click',()=>{
+        console.log('compraste'+boton.id)
+    })
+}
 }
 stockProd(productos)
 
-const adidasFilt = productos.filter((prods)=>prods.marca == 'Adidas')
-console.log(adidasFilt)
-stockProd(adidasFilt)
+let btnAdidas = document.getElementById('btn-adidas')
+btnAdidas.addEventListener('click', clickAdi)
 
-const nikeFilt = productos.filter((prods)=>prods.marca == 'Nike')
-console.log(nikeFilt)
+function clickAdi (){
+    clickAdi = productos.filter((prods)=>prods.marca == "Adidas")
+    console.log(clickAdi)
+    stockProd(clickAdi)
+}
 
-const pumaFilt = productos.filter((prods)=>prods.marca == 'Puma')
-console.log(pumaFilt)
+let btnNike = document.getElementById('btn-nike')
+btnNike.addEventListener('click', clickNik)
 
-const rebookFilt = productos.filter((prods)=>prods.marca == 'Rebook')
-console.log(rebookFilt)
+function clickNik (){
+    clickNik = productos.filter((prods)=>prods.marca == "Nike")
+    console.log(clickNik)
+    stockProd(clickNik)
+}
 
+let btnPuma = document.getElementById('btn-puma')
+btnPuma.addEventListener('click', clickPum)
 
-    
+function clickPum (){
+    clickPum = productos.filter((prods)=>prods.marca == "Puma")
+    console.log(clickPum)
+    stockProd(clickPum)
+}
+
+let btnRebook = document.getElementById('btn-rebook')
+btnRebook.addEventListener('click', clickReb)
+
+function clickReb (){
+    clickReb = productos.filter((prods)=>prods.marca == "Rebook")
+    console.log(clickReb)
+    stockProd(clickReb)
+}
+
 
 
